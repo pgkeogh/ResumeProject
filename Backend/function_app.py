1a5bdf3afbd3986515cf7e0b4f9aeb5867f1b2a1
pgkeogh <pgkeogh@hotmail.com>
2024-01-26 13:25:45 +1100

    Backend upload

---
+++ Backend/function_app.py
@@ -0,0 +1,33 @@
+import azure.functions as func
+import logging
+from azure.cosmos import CosmosClient
+import os
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="AzResumeTrigger")
+def AzResumeTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    endpoint = os.environ.get("COSMOS_ENDPOINT")
+    account_key = os.environ.get("COSMOS_KEY")
+
+    if not endpoint or not account_key:
+        raise ValueError("COSMOS_ENDPOINT and COSMOS_KEY must be set as environment variables.")
+  
+    database_name = "PKsVisitorCounter"
+    container_name = "VisitorCounterContainer"
+    client = CosmosClient(endpoint, account_key)
+    database = client.get_database_client(database_name)
+    container = database.get_container_client(container_name)
+    query = f"SELECT * FROM c WHERE c.id = '1'"
+    items = list(container.query_items(query, enable_cross_partition_query=True))
+    if items:
+        count = items[0]['count']
+        count = str(int(count) + 1)
+        items[0]['count'] = count
+        container.replace_item(items[0]['id'], items[0])
+        return func.HttpResponse(count, status_code=200)
+    else:
+        return func.HttpResponse("Counter document not found", status_code=404)
+    
